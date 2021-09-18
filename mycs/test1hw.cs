4c4e3a3a5942500fd3322a6c62ba9ab3e8e7f483
Honza S. Copak <bonum.comunae@volny.cz>
2021-09-18 10:54:43 +0200

    Add files via upload

---
+++ mycs/test1hw.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace test1hw
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
